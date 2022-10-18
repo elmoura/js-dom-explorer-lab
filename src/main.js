aa13f2ae6fb4d62f6f27df6ba934aec9d3679255
Gabriel Moura <mourasb.gabriel@gmail.com>
2022-10-18 20:32:37 -0300

    [wip] feat: aula 1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const creditCardLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+export const setCardType = (cardType) => {
+  const colors = {
+    visa: ["#2D57F2", "#436D99"],
+    mastercard: ["#C69347", "#DF6F29"],
+    default: ["black", "grey"],
+  }
+
+  ccBgColor01.setAttribute("fill", colors[cardType][0])
+  ccBgColor02.setAttribute("fill", colors[cardType][1])
+  creditCardLogo.setAttribute("src", `cc-${cardType}.svg`)
+}
+
+window.setCardType = setCardType
